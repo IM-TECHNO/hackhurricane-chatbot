bc2baf2ee4616d69400dc7ab199d7dbaa92fe1eb
IM-TECHNO <raagul3105@gmail.com>
2024-01-04 23:01:07 +0530

    Pushed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 import google.generativeai as genai
-# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
 GOOGLE_API_KEY='AIzaSyD04Ap2sE9Dx4LPGiP1y5ldqZNIfgaT-DY'
 genai.configure(api_key=GOOGLE_API_KEY)
 
@@ -15,23 +14,12 @@ def index():
 
 @app.route('/chat', methods=['GET','POST'])
 def chat():
-    # Get the question from the request
     question = request.json['question']
-
-    # Define the model and prompt
     model = genai.GenerativeModel('gemini-pro')
-    prompt = f'''Answer this question very shortly : {question}.
+    prompt = f'''Answer this question shortly and be precise. DO NOT ANSWER IF IT INVOLVES NSFW CONTENT. Here is your question : {question}.
     '''
-
-    # Generate the response using the palm library
-    # completion = palm.generate_text(
-    #     model=model,
-    #     prompt=prompt,
-    #     temperature=0,
-    # )
     reply = model.generate_content(prompt)
 
-    # Return the response as JSON
     return jsonify({'response': reply.text})
 
 if __name__ == '__main__':
